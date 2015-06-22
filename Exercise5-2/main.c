1a6a65c857c186e7baeed762106368b80a64b933
Matt Quinn <matt@mattjquinn.com>
2015-06-21 18:57:14 -0700

    Completing Exercise5-2; prints "Hello World!" to ttyUSB0 via USART1 bridge.

--- Exercise5-2/main.c
+++ Exercise5-2/main.c
@@ -2,10 +2,49 @@
 #include <stm32f10x_usart.h>
 #include "uart.h"
 
+void Delay(uint32_t nTime);
+
 main() {
-  // Initialize usart/gpio clocks.
-  // Configure usart pins
-  // Configure and enable the usart1
+  // Iniialize timer tick.
+  if(SysTick_Config(SystemCoreClock / 1000)) {
+    while(1);
+  }
+
+  // Initialize the uart.
+  uart_open(9600, USART_Mode_Rx | USART_Mode_Tx);
+
+  // Write Hello World!\n\r'' every 250msec.
+  while(1) {
+    // TODO: Write Hello World to screen.
+    uart_putc('H');
+    uart_putc('e');
+    uart_putc('l');
+    uart_putc('l');
+    uart_putc('o');
+    uart_putc(' ');
+    uart_putc('W');
+    uart_putc('o');
+    uart_putc('r');
+    uart_putc('l');
+    uart_putc('d');
+    uart_putc('!');
+    uart_putc('\n');
+    uart_putc('\r');
+    Delay(250);
+  }
+}
+
+// Timer code
+static __IO uint32_t TimingDelay;
+
+void Delay(uint32_t nTime){
+  TimingDelay = nTime;
+  while(TimingDelay != 0);
+}
+
+void SysTick_Handler (void){
+  if (TimingDelay != 0x00)
+    TimingDelay--;
 }
 
 #ifdef USE_FULL_ASSERT
